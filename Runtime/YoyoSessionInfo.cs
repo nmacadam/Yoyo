7ef6959ddcafbc89e5d5d94810e0b4b5f75bcf6b
Unknown <nkmacadam@gmail.com>
2021-04-20 23:06:57 -0400

    🪀 added YoyoSessionInfo

---
+++ Runtime/YoyoSessionInfo.cs
@@ -0,0 +1,37 @@
+﻿// Yoyo Network Engine, 2021
+// Author: Nathan MacAdam
+
+using UnityEngine;
+
+namespace Yoyo.Runtime
+{
+	/// <summary>
+	/// Singleton access for critical networking info
+	/// </summary>
+	public class YoyoSessionInfo
+	{
+        private static object m_Lock = new object();
+        private static YoyoSession _internalReference;
+
+        private static YoyoSession _instance
+        {
+            get
+            {
+                lock (m_Lock)
+                {
+                    if (_internalReference == null)
+                    {
+                        _internalReference = GameObject.FindObjectOfType<YoyoSession>();
+                    }
+
+                    return _internalReference;
+                }
+            }
+        }
+
+		public static YoyoEnvironment Environment => _instance.Environment;
+		public static bool IsActive => _instance.Environment != YoyoEnvironment.None;
+		public static bool IsClient => _instance.Environment == YoyoEnvironment.Client;
+		public static bool IsServer => _instance.Environment == YoyoEnvironment.Server;
+	}
+}
